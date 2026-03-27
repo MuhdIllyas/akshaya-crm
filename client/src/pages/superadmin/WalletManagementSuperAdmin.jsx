d124be24213f9a681e9a210ee7a1d17bf69ffc80
MuhdIllyas <muhdillyasks@gmail.com>
2026-03-27 19:48:30 +0530

    Update API URL to use environment variable

--- client/src/pages/superadmin/WalletManagementSuperAdmin.jsx
+++ client/src/pages/superadmin/WalletManagementSuperAdmin.jsx
@@ -153,7 +153,7 @@ const WalletManagementSuperAdmin = () => {
   // Fetch centres for superadmins
   const fetchCentres = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/api/centres", {
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/centres`, {
         headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
       });
       setCentres(response.data);
