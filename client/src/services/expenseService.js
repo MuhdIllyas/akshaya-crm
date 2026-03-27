fe25604cd48e238dc2da105b4ae94408c2af3a77
MuhdIllyas <muhdillyasks@gmail.com>
2026-03-27 19:14:17 +0530

    Update API base URL to use environment variable

--- client/src/services/expenseService.js
+++ client/src/services/expenseService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/expense',
+  baseURL: `${import.meta.env.VITE_API_URL}/api/expense`,
 });
 
 api.interceptors.request.use(config => {
