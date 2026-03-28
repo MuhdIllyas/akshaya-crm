a04c38e14d8aa644fcd3d042c94e3808dca26bc0
MuhdIllyas <muhdillyasks@gmail.com>
2026-03-28 18:39:19 +0530

    Fix baseURL formatting in salaryService

--- client/src/services/salaryService.js
+++ client/src/services/salaryService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-  baseURL: `import.meta.env.VITE_API_URL/api/salary`,  
+  baseURL: `${import.meta.env.VITE_API_URL}/api/salary`,  
 });
 
 api.interceptors.request.use(config => {
