890b78f6f7f6232e4bc7f6857b8c597aa33ee77a
MuhdIllyas <muhdillyasks@gmail.com>
2026-04-11 19:19:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -243,10 +243,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => {
-  res.send("API is running 🚀");
-});
-
 /* ================================
    SOCKET ACCESS IN ROUTES
 ================================ */
