934364df64bef75915e77e0d3547fbe284385048
MuhdIllyas <muhdillyasks@gmail.com>
2026-07-15 16:28:32 +0530

    fix: update import path for database configuration in notificationService

--- server/utils/notificationService.js
+++ server/utils/notificationService.js
@@ -1,4 +1,4 @@
-import pool from '../config/db.js';
+import pool from '../db.js';
 import socketNotifications from './socketNotifications.js';
 import { NOTIFICATION_TYPES } from './notificationTemplates.js';
 
