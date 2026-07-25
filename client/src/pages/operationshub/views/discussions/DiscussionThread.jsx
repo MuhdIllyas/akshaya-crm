40614abc5cb292a40ac81a0231a88d9bc040bc7c
MuhdIllyas <muhdillyasks@gmail.com>
2026-07-25 14:22:03 +0530

    feat: add useEffect import to DiscussionThread component for bookmark status handling

--- client/src/pages/operationshub/views/discussions/DiscussionThread.jsx
+++ client/src/pages/operationshub/views/discussions/DiscussionThread.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiChevronLeft, FiCheckCircle, FiUser, FiClock, FiMessageSquare, FiTarget, FiBriefcase, FiBookmark } from 'react-icons/fi';
 import { toast } from 'react-toastify';
 import { addDiscussionReply, markDiscussionSolved, toggleBookmark } from '@/services/knowledge';
